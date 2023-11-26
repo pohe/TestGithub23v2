2adf0955e279b9137f6b253ea895b2fc2a1bd562
Poul Henriksen <POHE@zealand.dk>
2023-11-26 13:36:51 +0100

    Added 23

--- TestGithub23/Program.cs
+++ TestGithub23/Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("dette er skevet af POHE");
 
-Console.WriteLine("dette er skevet af Luop2017");
+Console.WriteLine("dette er skevet af Luop2017 til 23");
